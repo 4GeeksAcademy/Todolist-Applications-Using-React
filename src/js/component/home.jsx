37a45257ee8b47e7ecab1e7a3da37d8a9f2d0a3a
deepak <160626769+Slayercube@users.noreply.github.com>
2024-05-16 17:01:11 +0000

    sidhu-commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,42 @@
-import React from "react";
-
-//include images into your bundle
+import React, { useState } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
+	const [test, setTest] = useState("");
+	const [newtask, setNewtask] = useState("");
+	const [todos, setTodos] = useState([]);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setTodos([...todos, { label: newtask, done: false }]);
+		setNewtask("");
+	};
+
+	const handleChange = (e) => setNewtask(e.target.value);
+
+	const handleDelete = (i) => {
+		let aux = todos;
+		aux = aux.filter((el, index) => i !== index);
+		setTodos(aux);
+	};
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="card">
+
+			
+			<form onSubmit={handleSubmit}>
+				<input type="text" value={newtask} onChange={handleChange} />
+				<button type="submit">Add Task</button>
+
+				<ul className="list-group list-group-flush" id="ulClass">
+					{todos.length >= 1 ? todos.map((el, i) => (
+						<li className="list-group-item" id="liId" key={i}>
+							{el.label}
+							<span onClick={() => handleDelete(i)}> <button className="btn btn-secondary">x</button></span>
+						</li>
+					)) : null}
+				</ul>
+			</form>
 		</div>
 	);
 };
